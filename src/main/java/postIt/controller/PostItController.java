9fb40a6c06491855701bc6cc1deb135bf890b590
Kaysson Gabriel <104291250+KayssonGabriel@users.noreply.github.com>
2024-08-09 15:56:30 -0300

    Update PostItController.java

--- src/main/java/postIt/controller/PostItController.java
+++ src/main/java/postIt/controller/PostItController.java
@@ -1,16 +1,23 @@
 package postIt.controller;
 
 import postIt.DAO.ExceptionDAO;
+import postIt.DAO.PostItDAO;
 import postIt.model.PostIt;
 
 import java.util.Date;
 
 public class PostItController {
+    private PostItDAO postItDAO;
+
+    public PostItController() {
+        this.postItDAO = new PostItDAO();
+    }
+
     public boolean savePostIt(String title, String description, Date deadline) {
         if (title != null && !title.isEmpty() && description != null && !description.isEmpty() && deadline != null) {
             PostIt postIt = new PostIt(title, description, deadline);
             try {
-                postIt.savePostIt(postIt);
+                postItDAO.savePostIt(postIt);
                 return true;
             } catch (ExceptionDAO e) {
                 e.printStackTrace();
